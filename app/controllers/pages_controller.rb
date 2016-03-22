6fbaa4e16080ac5908c890084df2bea048b04407
Francisca Medina <fbmedina@uc.cl>
2016-03-22 20:28:08 -0300

    Se hizo el formulario, se agarran los datos, todo ok hasta ahora

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,19 @@ class PagesController < ApplicationController
 
   def batman_vs_superman
   end
+
+  def save_superheroe
+    if params[:heroe].present? && params[:email].present?
+      Vote.create(
+          heroe: params[:heroe],
+          email: params[:email]
+          )
+
+      redirect_to root_path, notice: "Voto recibido"
+
+    else
+      flash[:alert] = "Complete todos los datos"
+      render :batman_vs_superman
+    end
+  end
 end
